7fb5dacaa2e03c1d2bc67e85da1bb7cde952349e
fismaili <45186231+fismaili@users.noreply.github.com>
2020-10-20 20:55:35 +0200

    Address added

---
+++ Models/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework_CodeFirst.Models
+{
+    public class Address
+    {
+        public string Sudent_Address { get; set; }
+    }
+}
