809b89ed1dd0bc16082f545936db9db811578a17
fismaili <45186231+fismaili@users.noreply.github.com>
2020-11-03 22:32:25 +0100

    Display Students List

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EntityFramework_CodeFirst.Models;
+
+namespace EntityFramework_CodeFirst.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext context;
+
+        public StudentsController(SchoolContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IActionResult> Students()
+        {
+            return View(await context.Students.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("StudentId,StudentName,DateOfBirth")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(student);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Students));
+            }
+            return View(student);
+        }
+    }
+}
